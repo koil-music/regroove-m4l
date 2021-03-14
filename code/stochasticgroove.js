53e3f77b5e4947b5c366810e17b84665a9b50b3a
mxkrn <maxkraan1@gmail.com>
2021-03-14 17:40:53 +0100

    stochasticgroove.js compatability with stochastic-groove-lib@1.3

--- code/stochasticgroove.js
+++ code/stochasticgroove.js
@@ -2,230 +2,231 @@
 
 const Max = require('max-api');
 const fs = require('fs');
+const path = require('path');
 
-const { PatternBuffer, Generator, DRUM_PITCH_CLASSES, pitchToIndexMap } = require('stochastic-groove-lib');
+const { Pattern } = require("stochastic-groove-lib/dist/pattern");
+const { PatternHistory } = require("stochastic-groove-lib/dist/history");
+const { Generator } = require("stochastic-groove-lib/dist/generate");
+const { readMidiFile } = require('stochastic-groove-lib/dist/midi');
+const { pitchToIndexMap } = require('stochastic-groove-lib/dist/util')
+const { CHANNELS, LOOP_DURATION, DRUM_PITCH_CLASSES } = require('stochastic-groove-lib/dist/constants');
 
-let basePatternBuffer;
-let activePatternBuffer;
+/**
+ * Pattern
+ */
+const dims = [1, LOOP_DURATION, CHANNELS];
+const zeroPattern = new Pattern(new Float32Array(dims[0] * dims[1] * dims[2]), dims)
+let onsetsPattern = zeroPattern;
+let velocitiesPattern = zeroPattern;
+let offsetsPattern = zeroPattern;
+let velocity;
+
+function updateCell(value, step, instrument) {
+    onsetsPattern.setcell(value, step, instrument);
+    velocitiesPattern.setcell(value, step, instrument);
+    offsetsPattern.setcell(0., step, instrument);
+}
+
+const patternHistory = new PatternHistory(20);
+
+/**
+ * MIDI Pattern Factory
+ */
+let onsetsMidiPattern = zeroPattern;
+let velocitiesMidiPattern = zeroPattern;
+let offsetsMidiPattern = zeroPattern;
+
+// TODO: Figure out clever way for universal MIDI pattern mapping
+const pitchMapping = pitchToIndexMap(DRUM_PITCH_CLASSES['pitch'], DRUM_PITCH_CLASSES['index']);
+
+async function readMidi(filename) {
+    [onsetsMidiPattern, velocitiesMidiPattern, offsetsMidiPattern] = await readMidiFile(filename, pitchMapping);
+    onsetsPattern = onsetsMidiPattern;
+    velocitiesPattern = velocitiesMidiPattern;
+    offsetsPattern = offsetsMidiPattern;
+    patternHistory.append(onsetsPattern);
+}
+
+/**
+ * Generate
+ */
+let numSamples = 400;
+let minOnsetThreshold = 0.3;
+let maxOnsetThreshold = 0.7;
+let noteDropout = 0.5;
+
+let modelDir = path.dirname(__dirname) + '/assets/models/'
 let generator;
 let generatorReady = false;
 let isGenerating = false;
-let syncing = false;
 
-let numSamples = 100;  // TODO: Elegant way to get this from Max
-let randomFactor = 0.5;
-let noteDropout = 0.5;
-
-Max.addHandler('readMidiFile', async(filePath) => {
-    // instantiate basePatternBuffer with MIDI file
-    const pitchIndexMap = pitchToIndexMap(DRUM_PITCH_CLASSES['pitch'], DRUM_PITCH_CLASSES['index']);
-    basePatternBuffer = await PatternBuffer.from_midi(filePath, pitchIndexMap);
-    activePatternBuffer = basePatternBuffer;
-
-    // Assign pattern to matrixCtrl and sync matrixCtrl with Max
-    // matrixCtrl.pattern = basePatternBuffer.pattern;
-    matrixCtrl.onsets = basePatternBuffer.onsets;
-    matrixCtrl.velocities = basePatternBuffer.velocities;
-    matrixCtrl.offsets = basePatternBuffer.offsets;
-    await matrixCtrl.sync(true);
-});
-
-Max.addHandler('deltaZ', async(z1, z2) => {
-    if (generatorReady) {
-        let input = `${z1},${z2}`;
-        try {
-            const onsets = generator.data[input].onsets;
-            const velocities = generator.data[input].velocities;
-            const offsets = generator.data[input].offsets;
-            let newPatternBuffer = new PatternBuffer(onsets, velocities, offsets);
-
-            activePatternBuffer = newPatternBuffer;
-
-            // matrixCtrl.pattern = newPatternBuffer.pattern;
-            matrixCtrl.onsets = newPatternBuffer.onsets;
-            matrixCtrl.velocities = newPatternBuffer.velocities;
-            matrixCtrl.offsets = newPatternBuffer.offsets;
-            // TODO: Fill preview matrixCtrl
-            // await matrixCtrl.sync(true);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-});
-
-Max.addHandler('sync', async(step) => {
-    if (step == 0) {
-        await matrixCtrl.sync(true);
-    }
-});
-
-Max.addHandler('setRandom', async(value) => {
-    randomFactor = value;
-});
-
-Max.addHandler('generate', async() => {
-    // Build Generator with the loaded pattern and populate the latent space
+async function generate() {
     if (!isGenerating) {
         isGenerating = true;
-        await Max.outlet('isGenerating');
-        // // let patternArray = Array.from(patternString).map(Number);
-        // basePatternBuffer = new PatternBuffer(onsets, velocities, offsets);
-
-        // // Assign pattern to matrixCtrl and sync matrixCtrl with Max
-        // matrixCtrl.pattern = basePatternBuffer.pattern;
-        // matrixCtrl.onsets = basePatternBuffer.onsets;
-        // matrixCtrl.velocities = basePatternBuffer.velocities;
-        // matrixCtrl.offsets = basePatternBuffer.offsets;
-        
-        // Build Generator with the loaded pattern and populate the latent space
         generator = await Generator.build(
-            activePattern,
-            activePatternBuffer.velocities,
-            activePatternBuffer.offsets,
-            numSamples, 
-            noteDropout, 
-            matrixCtrl.channels, 
-            matrixCtrl.loopDuration
+            onsetsPattern.data,
+            velocitiesPattern.data,
+            offsetsPattern.data,
+            modelDir,
+            minOnsetThreshold,
+            maxOnsetThreshold,
+            numSamples,
+            noteDropout,
+            CHANNELS,
+            LOOP_DURATION
         );
-        await generator.populate();
-        await Max.post('Generator is ready.');
+        await generator.run();
         generatorReady = true;
         isGenerating = false;
-        await Max.outlet('generatorReady');
-        await Max.outlet('isGenerating');
-    };
-});
-
-Max.addHandler('updatePattern', async(indexString) => {
-    /**
-     * Definitely need a better way of doing this then this horrible
-     * if else if clusterfuuuu
-     */
-    let step, channel, value;
-    if (indexString.length == 4) {
-        step = parseInt(indexString.slice(0,2));
-        channel = parseInt(indexString[2]);
-        value = parseInt(indexString[3]);
-    } else if (indexString.length == 2) {
-        step = 0;
-        channel = parseInt(indexString[0]);
-        value = parseInt(indexString[1]);
-    } else if (indexString.length == 1) {
-        step = 0;
-        channel = 0;
-        value = parseInt(indexString[0]);
-    } else {
-        step = parseInt(indexString[0]);
-        channel = parseInt(indexString[1]);
-        value = parseInt(indexString[2]);
-    };
-
-    // update playingPatternBuffer
-    let channelInverse = Object.keys(DRUM_PITCH_CLASSES['pitch']).length - 1 - channel;
-    activePattern[channelInverse][step] = value;
-    matrixCtrl.pattern = activePattern;
-    matrixCtrl.onsets = activePattern;
-});
-
-/**
- * MatrixCtrl communicates with the corresponding MatrixCtrl object on the Max side
- */
-class MatrixCtrl {
-    /**
-     * @loopDuration Number of time steps, shown as columns in the Max MatrixCtrl object
-     * @channels Number of instruments, shown as rows in the Max MatrixCtrl object
-     * @_pattern This contains the pattern that will be synced with
-     * the Max MatrixCtrl object when this.sync is called.
-     */
-    constructor() {
-        this.loopDuration = 32;
-        this.channels = 9;
-        this._pattern = this._initArray();
-        this._onsets = this._initArray();
-        this._velocities = this._initArray();
-        this._offsets = this._initArray();
-    }
-    set onsets(v) {
-        this._onsets = v;
-    }
-    get onsets() {
-        return this._onsets;
-    }
-    set velocities(v) {
-        this._velocities = v;
-    }
-    get velocities() {
-        return this._velocities;
-    }
-    set offsets(v) {
-        this._offsets = v;
-    }
-    get offsets() {
-        return this._offsets;
-    }
-    set pattern(p) {
-        this._pattern = p;
-    }
-    get pattern() {
-        return this._pattern;
-    }
-    async sync(force=false) {
-        /**
-         * Syncs pattern state with MatrixCtrl sibling in Max.
-         * To be called in an interval function so performance is key.
-         */
-        if (force) {
-            syncing = false;
-        };
-        if (!syncing) {
-            syncing = true;
-            const pattern = new Array;
-
-            const onsets = new Array;
-            const velocities = new Array;
-            const offsets = new Array;
-            for (let channel = 0; channel < this.channels; channel++) {
-                for (let step = 0; step < this.loopDuration; step++) {
-                    // Max MatrixCtrl is indexed from top -> bottom so we use the inverse channel
-                    pattern.push(step);
-                    let channelInverse = Object.keys(DRUM_PITCH_CLASSES['pitch']).length - 1 - channel;
-                    pattern.push(channelInverse);
-                    pattern.push(this._onsets[channel][step]);
-                    
-                    // fill jitter matrices
-                    velocities.push(this._velocities[channel][step]);
-                    // if (step == 31) { // TODO: Fix this bug in lib
-                    //     offsets.push(0.);
-                    // } else {}
-                    offsets.push(this._offsets[channel][step]);
-                    onsets.push(this._onsets[channel][step]);
-                }
-            }
-            // fill visual display
-            await Max.outlet("fillMatrixCtrl", ...pattern);
-
-            // fill Max-side data matrices
-            await Max.outlet("onsets", ...onsets);
-            await Max.outlet("velocities", ...velocities);
-            await Max.outlet("offsets", ...offsets);
-            syncing = false;
-        }
-    }
-    _initArray() {
-        return Array.from({ length: this.channels }, _ => {
-            let array = Array.from({ length: this.loopDuration }, _ => 0);
-            return array;
-        });
+        Max.post('Generator is ready.');
     }
 }
 
-// Initialize MatrixCtrl
-const matrixCtrl = new MatrixCtrl();
-basePatternBuffer = new PatternBuffer(matrixCtrl.onsets, matrixCtrl.velocities, matrixCtrl.offsets);
-activePatternBuffer = basePatternBuffer;
-let activePattern = activePatternBuffer.onsets;
+/**
+ * MatrixCtrl
+ */
+let densityIndex;
+let syncMode = "off";
+let syncRate = 16.; // in sixteenth notes
+let isSyncing = false;
+let previewOnsetsPattern = onsetsPattern;
 
-// Default zero arrays
-matrixCtrl.onsets = activePattern;
-matrixCtrl.velocities = activePattern;
-matrixCtrl.offsets = activePattern;
+function updatePattern() {
+    if (generatorReady) {
+        const randomIndex = Math.round(Math.random()*generator.axisLength);
+        patternHistory.append(onsetsPattern);
+        onsetsPattern = generator.onsets.sample(densityIndex, randomIndex);
+        // TODO: Predict velocities and offsets for each pattern in matrix
+        // velocitiesPattern = generator.velocities.sample(densityIndex, randomIndex);
+        // offsetsPattern = generator.offsets.sample(densityIndex, randomIndex);
+    } else {
+        // TODO: Generator not trained warning
+    }
+}
+
+function createMatrixCtrlData() {
+    const data = [];
+    for (let channel = 8; channel >= 0; channel--) {
+        for (let step = 0; step < LOOP_DURATION; step++) {
+            data.push(step);
+            data.push(channel);
+            data.push(onsetsPattern[channel][step]);
+        }
+    }
+    return data;
+}
+
+async function waitSync (step) {
+    if (!isSyncing  && syncMode === "sync") {
+        if (step % syncRate === syncRate - 1) {
+            isSyncing = true;
+
+            updatePattern();
+            const matrixCtrl = createMatrixCtrlData();
+            
+            await Max.outlet("fillMatrixCtrl", ...matrixCtrl);
+            isSyncing = false;
+        }
+    }
+}
+
+async function snapSync () {
+    if (!isSyncing && syncMode === "snap") {
+        isSyncing = true;
+        updatePattern()
+
+        const matrixCtrl = createMatrixCtrlData();
+        await Max.outlet("fillMatrixCtrl", ...matrixCtrl);
+        isSyncing = false;
+    }
+}
+
+const print = (value) => {
+    Max.post(value)
+}
+
+/**
+ * Max request handlers
+ */
+Max.addHandler("set_density", (value) => {
+    if (value >= 0 && value <= 1) {
+        densityIndex = Math.round(value * Math.sqrt(numSamples));
+    } else {
+        Max.post(`invalid density value ${value} - must be between 0 and 1`)
+    }
+})
+Max.addHandler("set_max_density", (value) => {
+    if (value >= 0 && value <= 1) {
+        maxOnsetThreshold = 1 - value;
+    } else {
+        Max.post(`invalid maxOnsetThreshold value ${value} - must be between 0 and 1`)
+    }
+})
+Max.addHandler("set_min_density", (value) => {
+    if (value >= 0 && value <= 1) {
+        minOnsetThreshold = 1 - value;
+    } else {
+        Max.post(`invalid minOnsetThreshold value ${value} - must be between 0 and 1`)
+    }
+})
+Max.addHandler("set_note_dropout", (value) => {
+    if (value >= 0 && value <= 1) {
+        noteDropout = value;
+    } else {
+        Max.post(`invalid noteDropout value ${value} - must be between 0 and 1`)
+    }
+})
+Max.addHandler("set_num_samples", (value) => {
+    if (value >= 0 && value <= 1000) {
+        noteDropout = value;
+    } else {
+        Max.post(`invalid numSamples value ${value} - must be between 0 and 1000`)
+    }
+})
+
+const syncModeOptions = ["snap", "off", "wait"];
+Max.addHandler("set_sync_mode", (value) => {
+    if (value in syncModeOptions) {
+        syncMode = value;
+    } else {
+        Max.post(`invalid syncMode ${value} - must be one of ${options}`)
+    }
+})
+
+const syncRateOptions = [0.25, 0.5, 1., 2., 4.]
+Max.addHandler("set_sync_rate", (value) => {
+    if (value in syncRateOptions) {
+        syncRate = value;
+    } else {
+        Max.post(`invalid syncMode ${value} - must be one of ${options}`)
+    }
+})
+Max.addHandler("set_velocity", (value) => {
+    if (value >= 0 && value <= 1) {
+        velocity = value;
+    } else {
+        Max.post(`invalid velocity value ${value} - must be between 0 and 1`)
+    }
+})
+Max.addHandler("generate", () => {
+    generate();
+})
+Max.addHandler("snap_sync", () => {
+    snapSync();
+});
+Max.addHandler("update_cell", (value, step, instrument) => {
+    if (value in [0., 1.]) {
+        if (step < LOOP_DURATION && instrument < CHANNELS) {
+            updateCell(value, step, instrument);
+        } else {
+            Max.post(`Invalid pattern index: [${step}, ${instrument}]`)
+        }
+    }
+    
+})
+Max.addHandler("wait_sync", (step) => {
+    waitSync(step);
+})
+Max.addHandler("setModelDir", (value) => {
+  modelDir = value;
+})
