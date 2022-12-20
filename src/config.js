40ea661e6be717cee1277d61280954880bfab709
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2022-12-20 22:26:13 +0100

    Move MODEL_DIR and DEBUG into config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+const process = require("process");
+const path = require("path");
+
 const BUFFER_LENGTH = 512;
 const LOOP_DURATION = 16;
 const MIN_ONSET_THRESHOLD = 0.3;
@@ -7,12 +10,27 @@ const NUM_INSTRUMENTS = 9;
 const NOTE_UPDATE_THROTTLE = 250; // milliseconds
 const TICKS_PER_16TH = BUFFER_LENGTH / LOOP_DURATION;
 
+const ROOT = path.dirname(process.cwd());
+let MODEL_DIR;
+if (process.env.MAX_ENV == "max") {
+  MODEL_DIR = path.join(ROOT, "regroove-models/current");
+} else {
+  MODEL_DIR = path.join(ROOT, "current");
+}
+
+let DEBUG = true;
+if (process.env.MAX_ENV == "maxforlive") {
+  DEBUG = false;
+}
+
 module.exports = {
   BUFFER_LENGTH,
+  DEBUG,
   LOOP_DURATION,
   MIN_ONSET_THRESHOLD,
   MAX_ONSET_THRESHOLD,
   MAX_VELOCITY,
+  MODEL_DIR,
   NUM_INSTRUMENTS,
   NOTE_UPDATE_THROTTLE,
   TICKS_PER_16TH,
