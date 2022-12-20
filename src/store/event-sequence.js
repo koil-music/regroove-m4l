785aa3a154b26e67fc4658640abd9278f7322d5b
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2022-12-20 09:54:14 +0100

    Update EventSequence and note-event.js

--- src/store/event-sequence.js
+++ src/store/event-sequence.js
@@ -1,133 +1,14 @@
-const Max = require("max-api");
+const Max = require("../max-api");
 const { makeAutoObservable, reaction } = require("mobx");
-
-const {
-  BUFFER_LENGTH,
-  TICKS_PER_16TH,
-  MAX_VELOCITY,
-  NOTE_UPDATE_THROTTLE,
-  NUM_INSTRUMENTS,
-  LOOP_DURATION,
-} = require("../config");
-
-class NoteEvent {
-  constructor(
-    instrumentIndex,
-    step,
-    onsetValue,
-    velocityValue,
-    offsetValue,
-    globalVelocity,
-    globalDynamics,
-    globalDynamicsOn,
-    globalMicrotiming,
-    globalMicrotimingOn,
-    velAmp,
-    velRand,
-    timeRand,
-    timeShift
-  ) {
-    // assign input values as class variables
-    this.instrumentIndex = instrumentIndex;
-    this.step = step;
-    this.onsetValue = onsetValue;
-    this.velocityValue = velocityValue;
-    this.offsetValue = offsetValue;
-    this.globalMicrotiming = globalMicrotiming;
-    this.globalMicrotimingOn = globalMicrotimingOn;
-    this.globalDynamics = globalDynamics;
-    this.globalDynamicsOn = globalDynamicsOn;
-    this.globalVelocity = globalVelocity;
-    this.velAmp = velAmp;
-    this.velRand = velRand;
-    this.timeRand = timeRand;
-    this.timeShift = timeShift;
-  }
-
-  get instrument() {
-    return NUM_INSTRUMENTS - 1 - this.instrumentIndex;
-  }
-
-  get quantizedTick() {
-    return this.step * TICKS_PER_16TH;
-  }
-
-  get tickRange() {
-    return {
-      min: this.quantizedTick - TICKS_PER_16TH / 2,
-      max: this.quantizedTick + TICKS_PER_16TH / 2,
-    };
-  }
-
-  get tick() {
-    let offsetValue = 0;
-    if (this.globalMicrotimingOn) {
-      // scale the predicted offsetValue by globalMicrotiming
-      offsetValue = this.offsetValue * this.globalMicrotiming;
-
-      // shift offsetValue by timeShift
-      offsetValue += this.timeShift;
-
-      // shift offsetValue by random timeShift
-      const randTimeShift = this.timeRand * (Math.random() - 0.5);
-      offsetValue += randTimeShift;
-    }
-
-    // calculate tick
-    let tick = this.quantizedTick + (offsetValue * TICKS_PER_16TH) / 2;
-
-    // check if offsetValue is within allowed range
-    if (tick < this.tickRange.min) {
-      tick = this.tickRange.min;
-    } else if (tick > this.tickRange.max) {
-      tick = this.tickRange.max;
-    }
-
-    // wrap around
-    if (tick < 0) {
-      return BUFFER_LENGTH + tick;
-    } else {
-      return tick;
-    }
-  }
-
-  get velocity() {
-    let velocity = this.velocityValue;
-    if (this.globalDynamicsOn) {
-      // scale the predicted velocityValue by globalDynamics
-      velocity *= this.globalDynamics;
-    }
-    // add global velocity
-    velocity *= this.globalVelocity;
-
-    // add detail to velocity
-    const randVelAmp = this.velRand * (Math.random() - 0.5);
-    velocity += randVelAmp;
-    velocity += this.velAmp * velocity;
-
-    // check if velocity is within allowed range
-    if (velocity < 0) {
-      velocity = 0.0;
-    } else if (velocity > 1) {
-      velocity = 1.0;
-    }
-    return velocity * MAX_VELOCITY;
-  }
-}
-
-class BufferEvent {
-  constructor(pitch, velocity) {
-    this.pitch = pitch;
-    this.velocity = velocity;
-  }
-
-  get array() {
-    return [this.pitch, this.velocity];
-  }
-}
+const NoteEvent = require("./note-event");
+const { NUM_INSTRUMENTS, LOOP_DURATION, BUFFER_LENGTH } = require("../config");
 
 class EventSequence {
-  constructor(numInstruments, loopDuration, bufferLength) {
+  constructor(
+    numInstruments = NUM_INSTRUMENTS,
+    loopDuration = LOOP_DURATION,
+    bufferLength = BUFFER_LENGTH
+  ) {
     this.numInstruments = numInstruments;
     this.loopDuration = loopDuration;
     this.bufferLength = bufferLength;
@@ -135,13 +16,11 @@ class EventSequence {
   }
 
   reset() {
-    // public
     this.quantizedData = this._resetQuantizedData(this.loopDuration);
-    this.bufferData = this._initBufferData(0, this.bufferLength);
+    this.bufferData = this._resetBufferData(0, this.bufferLength);
   }
 
   _resetQuantizedData(length) {
-    // private
     const data = [];
     for (let i = 0; i < length; i++) {
       data.push({});
@@ -149,43 +28,37 @@ class EventSequence {
     return data;
   }
 
-  _initBufferData(start, end) {
-    // private
+  _resetBufferData(start, end) {
     const data = {};
     for (let i = start; i < end; i++) {
-      data[i] = new BufferEvent(-1, 0).array;
+      data[i] = {};
+      for (let j = 0; j < this.numInstruments; j++) {
+        data[i][j] = 0;
+      }
     }
     return data;
   }
 
   update(event) {
-    // public
-    const events = this.quantizedData[event.step];
-    const previousEvent = events[event.instrument];
-    const bufferDataUpdates = {};
-    if (event.onset === 1) {
-      events[event.instrument] = event;
-    } else {
-      // event.onset === 0
-      delete events[event.instrument];
-      if (previousEvent !== undefined) {
-        bufferDataUpdates[previousEvent.bufferIndex] = new BufferEvent(
-          previousEvent.instrument,
-          0
-        ).array;
-      }
-    }
-    this.quantizedData[event.step] = events;
+    const currentNoteEvents = this.quantizedData[event.step];
 
-    for (const e of Object.values(events)) {
-      const bufferEvent = new BufferEvent(e.instrument, e.velocity);
-      if (Object.keys(bufferDataUpdates).includes(e.bufferIndex.toString())) {
-        bufferDataUpdates[e.bufferIndex].push(...bufferEvent.array);
-      } else {
-        bufferDataUpdates[e.bufferIndex] = bufferEvent.array;
-      }
+    // check previousEvent and delete it from bufferData if it exists
+    const previousEvent = currentNoteEvents[event.instrument];
+    if (previousEvent !== undefined) {
+      this.bufferData[previousEvent.tick][event.instrument] = 0;
     }
-    return bufferDataUpdates;
+
+    // update quantizedData
+    if (event.onsetValue === 1) {
+      currentNoteEvents[event.instrument] = event;
+    } else if (event.onsetValue === 0) {
+      delete currentNoteEvents[event.instrument];
+    }
+
+    // update bufferData with new events
+    for (const e of Object.values(currentNoteEvents)) {
+      this.bufferData[e.tick][e.instrument] = e.velocity;
+    }
   }
 }
 
@@ -199,22 +72,19 @@ class EventSequenceHandler {
     // i.e. quantizedEventSequence = [{"36": [2, 100], "42": [0, 127]}, ...]
     makeAutoObservable(this);
     this.root = rootStore;
-    this.eventSequence = new EventSequence(
-      NUM_INSTRUMENTS,
-      LOOP_DURATION,
-      BUFFER_LENGTH
-    );
+    this.ignoreNoteUpdate = false;
 
     this.reactToParamsChange = reaction(
       () => this.root.uiParamsStore.expressionParams,
       (params) => {
-        this.updateSequence(
+        this.updateAll(
           this.root.patternStore.currentOnsets.tensor()[0],
+          params.velocity,
           params.dynamics,
-          params.microtiming,
-          params.velAmpDict,
           params.dynamicsOn,
-          params.microtimingOn
+          params.microtiming,
+          params.microtimingOn,
+          params.velAmpDict
         );
       }
     );
@@ -223,28 +93,20 @@ class EventSequenceHandler {
       () => this.root.patternStore.currentOnsets,
       (currentOnsets) => {
         // only trigger updateSequence if isPatternUpdate flag is set
-        if (this.isPatternUpdate) {
-          this.updateSequence(
+        if (!this.ignoreNoteUpdate) {
+          this.updateAll(
             currentOnsets.tensor()[0],
+            this.root.uiParamsStore.velocity,
             this.root.uiParamsStore.dynamics,
-            this.root.uiParamsStore.microtiming,
-            this.root.uiParamsStore.velAmpDict,
             this.root.uiParamsStore.dynamicsOn,
-            this.root.uiParamsStore.microtimingOn
+            this.root.uiParamsStore.microtiming,
+            this.root.uiParamsStore.microtimingOn,
+            this.root.uiParamsStore.velAmpDict
           );
-          this.togglePatternUpdate();
           this.toggleIgnoreNoteUpdate();
         }
       }
     );
-  }
-
-  reset() {
-    this.quantizedEventSequence.reset();
-  }
-
-  togglePatternUpdate() {
-    this.isPatternUpdate = !this.isPatternUpdate;
   }
 
   toggleIgnoreNoteUpdate() {
@@ -316,4 +178,4 @@ class EventSequenceHandler {
   }
 }
 
-module.exports = { EventSequenceHandler, NoteEvent };
+module.exports = { EventSequence, EventSequenceHandler };
---
+++ src/store/note-event.js
@@ -0,0 +1,113 @@
+const {
+  BUFFER_LENGTH,
+  TICKS_PER_16TH,
+  MAX_VELOCITY,
+  NUM_INSTRUMENTS,
+} = require("../config");
+
+class NoteEvent {
+  constructor(
+    instrumentIndex,
+    step,
+    onsetValue,
+    velocityValue,
+    offsetValue,
+    globalVelocity,
+    globalDynamics,
+    globalDynamicsOn,
+    globalMicrotiming,
+    globalMicrotimingOn,
+    velAmp,
+    velRand,
+    timeRand,
+    timeShift
+  ) {
+    // assign input values as class variables
+    this.instrumentIndex = instrumentIndex;
+    this.step = step;
+    this.onsetValue = onsetValue;
+    this.velocityValue = velocityValue;
+    this.offsetValue = offsetValue;
+    this.globalMicrotiming = globalMicrotiming;
+    this.globalMicrotimingOn = globalMicrotimingOn;
+    this.globalDynamics = globalDynamics;
+    this.globalDynamicsOn = globalDynamicsOn;
+    this.globalVelocity = globalVelocity;
+    this.velAmp = velAmp;
+    this.velRand = velRand;
+    this.timeRand = timeRand;
+    this.timeShift = timeShift;
+  }
+
+  get instrument() {
+    return NUM_INSTRUMENTS - 1 - this.instrumentIndex;
+  }
+
+  get quantizedTick() {
+    return this.step * TICKS_PER_16TH;
+  }
+
+  get tickRange() {
+    return {
+      min: this.quantizedTick - TICKS_PER_16TH / 2 + 1,
+      max: this.quantizedTick + TICKS_PER_16TH / 2,
+    };
+  }
+
+  get tick() {
+    let offsetValue = 0;
+    if (this.globalMicrotimingOn) {
+      // scale the predicted offsetValue by globalMicrotiming
+      offsetValue = this.offsetValue * this.globalMicrotiming;
+
+      // shift offsetValue by timeShift
+      offsetValue += this.timeShift;
+
+      // shift offsetValue by random timeShift
+      const randTimeShift = this.timeRand * (Math.random() - 0.5);
+      offsetValue += randTimeShift;
+    }
+
+    // calculate tick
+    let tick = this.quantizedTick + (offsetValue * TICKS_PER_16TH) / 2;
+
+    // check if offsetValue is within allowed range
+    if (tick < this.tickRange.min) {
+      tick = this.tickRange.min;
+    } else if (tick > this.tickRange.max) {
+      tick = this.tickRange.max;
+    }
+
+    // wrap around
+    if (tick < 0) {
+      return BUFFER_LENGTH + tick;
+    } else {
+      return tick;
+    }
+  }
+
+  get velocity() {
+    let velocity = this.velocityValue;
+    if (this.globalDynamicsOn) {
+      // scale the predicted velocityValue by globalDynamics
+      velocity *= this.globalDynamics;
+    }
+    // add global velocity
+    velocity *= this.globalVelocity;
+
+    // add detail to velocity
+    const randVelAmp = this.velRand * (Math.random() - 0.5);
+    velocity += randVelAmp;
+    velocity += this.velAmp * velocity;
+
+    // check if velocity is within allowed range
+    if (velocity < 0) {
+      velocity = 0.0;
+    } else if (velocity > 1) {
+      velocity = 1.0;
+    }
+    return velocity * MAX_VELOCITY;
+  }
+}
+
+module.exports = NoteEvent;
